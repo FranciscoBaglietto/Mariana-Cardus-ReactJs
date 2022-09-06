2aa8992fcacd0305a4c35309bd711983f9cc482e
FranciscoBaglietto <fbagliettopiermattei@gmail.com>
2022-09-06 19:18:42 -0300

    imagenes

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,36 +1,30 @@
-const Data =[{
-    id:1,
-    nombre: "Agua Micelar",
-    precio: 100,
-    img:"./../../asset/image/agua-micelar",
-    stock:10
-},
-{
-    id:2,
-    nombre: "Antiage",
-    precio: 200,
-    img:"../../asset/image/antiage",
-    stock:10
-},
-{
-    id:3,
-    nombre: "Crema de Manos",
-    precio: 300,
-    img:"../../asset/image/crema-de-manos",
-    stock:10
-},
+const Data = [
+    {
+        id: 1,
+        nombre: "Antiage",
+        precio: 200,
+        img: "/antiage.jpg",
+        stock: 10
+    },
+    {
+        id: 2,
+        nombre: "Crema de Manos",
+        precio: 300,
+        img: "/crema-de-manos.jpg",
+        stock: 10
+    },
 ]
 
 //promesa
 const getFetch = new Promise((res, rej) => {
     let condition = true
 
-    if(condition){
+    if (condition) {
         setTimeout(() => {
             res(Data)
         }, 2000);
     }
-    else{
+    else {
         rej(console.log("No hay datos de productos"))
     }
 
