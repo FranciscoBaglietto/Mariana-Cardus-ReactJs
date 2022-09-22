bc726114b783bdcf143112270426c07c77e961b2
FranciscoBaglietto <fbagliettopiermattei@gmail.com>
2022-09-22 18:37:24 -0300

    css a cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,34 +1,40 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/cartContext"
+import '../Cart/Cart.css'
 
 const Cart = () => {
     const { cart, addToCart, removeItem } = useContext(CartContext);
     console.log('cart', cart)
 
     return (
-        <div>
+        <>
             <h1>Carrito de Compra</h1>
-            {cart.length === 0 ? (
-                <>
-            <h2>No hay productos en tu carrito</h2>
-            <Link to={'/'}>Volver a Comprar</Link>
-                </>
-            ) : (
-                <>
-                    {cart.map((item) => (
-                        <div key={item.id}>
-                            <img src={item.img} width='100'></img>
-                            <h3>{item.nombre}</h3>
-                            <p>${item.precio}</p>
-                            <p>{item.cantidad}</p>
-                            <button onClick={() => removeItem(item.id)}>Eliminar producto</button>
-                            <p>Total: ${item.cantidad * item.precio}</p>
-                        </div>
-                    ))}
-                </>
-            )}
-        </div>
+            
+            <div className="mainCart">
+                {cart.length === 0 ? (
+                    <>
+                        <h2>No hay productos en tu carrito</h2>
+                        <Link to={'/'}>Volver a Comprar</Link>
+                    </>
+                ) : (
+                    <>
+                        {cart.map((item) => (
+
+                            <div className="contenedorCart" key={item.id}>
+                                <img className="imgCarrito" src={item.img} width='100'></img>
+                                <h3 className="nombreProducto">{item.nombre}</h3>
+                                <p>${item.precio}</p>
+                                <p>{item.cantidad}</p>
+                                <button onClick={() => removeItem(item.id)}>Eliminar producto</button>
+                                <p>Total: ${item.cantidad * item.precio}</p>
+                            </div>
+
+                        ))}
+                    </>
+                )}
+            </div>
+        </>
     )
 }
 
