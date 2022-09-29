ed9d2c839d14b9e665f7f8de30c04b76b6b6ef44
FranciscoBaglietto <fbagliettopiermattei@gmail.com>
2022-09-29 14:15:21 -0300

    firebase erres

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,49 +1,35 @@
 import ItemDetail from '../ItemDetail/ItemDetail'
-import Data from '../Data/Data';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const ItemDetailContainer = () => {
     const { id } = useParams();
-    const [producos, setProductos] = useState([]);
-    const [loading, setLoading] = useState(true)
+    const [productos, setProductos] = useState([]);
 
-
-    const dataBase = getFirestore()
-
+    const dataBase = getFirestore();
     const queryDoc = doc(dataBase, 'items', id);
 
-   
 
-    const getProduct = new Promise((resolve) => {
-        setTimeout(() => {
-           /* resolve(Data.filter((prod) => prod.id === id))*/
-           getDoc(queryDoc)
-           .then((res) => {
-               setProductos(res.data())
-           })
-           .catch((err) => console.log(err));
-       
-           console.log(data);
-        }, 2000)
-    })
+    const getProduct = () => {
+
+        getDoc(queryDoc).then((res) => {
+            setProductos({ id: res.id, ...res.data() });
+        }).catch((err) => console.log(err));
+    }
+
+
 
     useEffect(() => {
-        getProduct
-            .then((resp) => setData(resp))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false))
-    }, [id]);
+        getProduct()
+        /*.then((resp) => setData(resp))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))*/
+    });
 
     return (
         <>
-
-            {
-                loading ? <span>Cargando...</span> :
-                    <ItemDetail item={data}></ItemDetail>
-            }
-
+            <ItemDetail item={productos}></ItemDetail>
         </>
     )
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,5 @@
 import './ItemListContainer.css'
-import Data from '../Data/Data';
+//import Data from '../Data/Data';
 import { useEffect, useState } from 'react';
 import ItemList from '../ItemList/ItemList';
 import { useParams } from 'react-router-dom';
@@ -10,66 +10,57 @@ import { getFirestore, getDocs, collection, query, where } from "firebase/firest
 
 
 const ItemListContainer = ({ greeting }) => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
+    const [productoLista, setProductosLista] = useState([])
     const { categoryName } = useParams()
 
 
 
     //promesa
-    const getFetch = new Promise((res, rej) => {
-        let condition = true
+    const getFetch = () => {
+        const db = getFirestore();
+        const querySnapshot = collection(db, 'items')
+        if (categoryName) {
+            const queryFilter = query(querySnapshot, where("categotyId", "==", categoryName))
+            if (categoryName) {
+                getDocs(queryFilter).then((res) => {
+                    const list = res.docs.map((productos) => {
+                        return { id: productos.id, ...productos.data() }
+                    });
+                    setProductosLista(list);
+                })
+            } else {
+                getDocs(querySnapshot).then((res) => {
+                    const list = res.docs.map((productos) => {
+                        return { id: productos.id, ...productos.data() }
+                    });
+                    setProductosLista(list);
+                })
+            }
 
-        if (condition) {
-            setTimeout(() => {
-                const db = getFirestore();
-                const querySnapshot = collection(db, 'items')
-                const queryFilter = query(querySnapshot, where("categotyId", "==", categoryName))
-                if(categoryName) {
-                    getDocs(queryFilter).then((res) => {
-                        const list = res.docs.map((productos) => {
-                            return { id: productos.id, ...productos.data() }
-                        });
-                        setData(list);
-                    })
-                } else{
-                    getDocs(querySnapshot).then((res) => {
-                        const list = res.docs.map((productos) => {
-                            return { id: productos.id, ...productos.data() }
-                        });
-                        setData(list);
-                    })
-                }
-                
-                
-            }, 2000);
+
         }
-    })
+    }
 
 
     useEffect(() => {
-        getFetch
-            .then((resp) => {
-                if (categoryName) {
-                    const response = resp.filter((response) => response.category === categoryName)
-                    setData(response);
-                } else {
-                    setData(resp)
-                }
-            })
-            .catch((err) => setData(err))
-            .finally(() => setLoading(false))
+        getFetch();
+        /*  .then((resp) => {
+              if (categoryName) {
+                  const response = resp.filter((response) => response.category === categoryName)
+                  setData(response);
+              } else {
+                  setData(resp)
+              }
+          })
+          .catch((err) => setData(err))
+          .finally(() => setLoading(false))*/
     }, [categoryName])
 
 
     return (
         <>
             <h1 className='titulo'>{greeting}</h1>
-            {
-                loading ? <span>Cargando...</span> :
-                    <ItemList productos={data}></ItemList>
-            }
-
+            <ItemList productos={productoLista}></ItemList>
 
         </>
     )
